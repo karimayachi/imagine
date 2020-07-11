f7dc88fa3ff8877bda6c197b87c958bddea8bb09
Karim Ayachi <karim@spilter.nl>
2020-07-11 09:57:34 +0200

    updated demo

--- demo/index.ts
+++ demo/index.ts
@@ -7,26 +7,73 @@ import '@material/mwc-textfield';
 import '@material/mwc-list/mwc-list';
 import '@material/mwc-list/mwc-list-item';
 
-class ViewModel {
+class PeopleViewModel {
+    @observable html: string;
+    @observable people: Person[];
+    @observable selectedPerson: Person;
+
+    constructor() {
+        this.html = '';
+        this.people = [new Person('Karim', 'Ayachi', 1), new Person('John', 'Doe', 2)];
+        this.selectedPerson = this.people[0];
+    }
+
+    add = (): void => {
+        let newPerson: Person = new Person('', '', this.people.length + 1);
+        this.people.push(newPerson);
+        this.selectedPerson = newPerson; 
+    };
+
+    delete = (person: Person): void => {
+        this.people.remove(person);
+    }
+}
+
+class Person {
+    @observable id: number;
+    @observable firstname: string;
+    @observable lastname: string;
+    @observable retired: boolean;
+
+    constructor(firstname: string, lastname: string, id: number) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.id = id;
+        this.retired = false;
+    }
+}
+
+class MainViewModel {
     @observable name: string;
     @observable age: number;
     @observable genres: string[];
     @observable isDraggable: boolean;
     @observable happyIcon: string;
+    @observable selectedVM?: PeopleViewModel;
 
     constructor() {
         this.name = 'Karim';
         this.age = 41;
         this.genres = ['Hip Hop'];
         this.isDraggable = true;
-        this.happyIcon = "edit";
+        this.happyIcon = 'edit';
     }
 
     reset = (): void => {
         this.name = 'Karim';
         this.age = 41;
         this.isDraggable = false;
-        this.happyIcon = "shopping_cart";
+        this.happyIcon = 'shopping_cart';
+    }
+
+    showPeopleView = (): void => {
+        this.selectedVM = new PeopleViewModel();
+
+        fetch(`./views/demo2.html`).then((response: Response): void => {
+            response.text().then((text: string): void => {
+                this.selectedVM!.html = text;
+            });
+        });
     }
 
     addGenre = (): void => {
@@ -39,4 +86,4 @@ class ViewModel {
     }
 }
 
-bind(document.getElementById('bindthis'), new ViewModel());
+bind(document.getElementById('bindthis'), new MainViewModel());
